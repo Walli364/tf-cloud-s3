7dc774b6e5ee845e3c25e55bd5a20338ebc9243c
Waleed <waleedwalli364@gmail.com>
2026-05-20 11:44:20 +0500

    s3-bucket

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.40.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.8.1"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "ap-northeast-1"
+}
+
+resource "random_id" "rand_id" {
+  byte_length = 8
+}
+
+resource "aws_s3_bucket" "demo-bucket" {
+  bucket = "tf-cloud-bucket-${random_id.rand_id.hex}"
+}
+
+
+output "random" {
+  value = random_id.rand_id.hex
+}
